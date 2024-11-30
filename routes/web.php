a77b537c118bec47698a20db0d14ce15731165ff
frandg06 <fdiez86@gmail.com>
2024-11-30 04:06:16 +0100

    php info

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect("https://www.hookyapp.es/");
+    echo phpinfo();
+    // return redirect("https://www.hookyapp.es/");
 });
