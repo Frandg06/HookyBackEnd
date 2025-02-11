6d687942e4e34c007fbf9e97134b72235bd802a1
frandg06 <fdiez86@gmail.com>
2025-02-11 19:26:32 +0100

    feat: eliminar línea en blanco en el método getInterests de DomainController

--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -9,7 +9,6 @@ use Illuminate\Http\Request;
 class DomainController extends Controller
 {
     public function getInterests() {
-
         $insterest = Interest::all();
         return response()->json($insterest);
     }
