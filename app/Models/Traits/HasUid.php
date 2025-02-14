6a8f6574ba8210c05e08c92b96c1019d8fe8b71d
frandg06 <fdiez86@gmail.com>
2025-02-14 15:10:11 +0100

    fix: evitar la sobrescritura del UID al crear un modelo con UID existente

--- app/Models/Traits/HasUid.php
+++ app/Models/Traits/HasUid.php
@@ -7,6 +7,8 @@ trait HasUid
     protected static function bootHasUid()
     {
         static::creating(function ($model) {
+            if($model->uid) return;
+            
             $model->uid = Str::uuid();
         });
     }
