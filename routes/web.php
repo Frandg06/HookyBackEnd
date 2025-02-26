6db3798977bfbee72e5035ccde5c8905fce391c1
frandg06 <fdiez86@gmail.com>
2025-02-26 11:59:47 +0100

    feat: agregar ruta de verificación de estado del servidor

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ Route::get('/', function () {
     return redirect("https://www.hookyapp.es/");
 });
 
-Route::get('/email', function () {
-    return view('emails.recovery_password_app', [
-        'link' => 'link',
-        'name' => 'name',
-        'uid' => 'uid',
-    ]);
+Route::get('/health', function () {
+    echo "The server is up!";
 });
