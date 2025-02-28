8d5c17ae7689b29cfcf44878ca87595a05ea9f5c
frandg06 <fdiez86@gmail.com>
2025-02-28 12:22:09 +0100

    fix: cambiar tipo de columna user_uid a uuid en la tabla user_images

--- database/migrations/2024_10_23_162443_create_user_images_table.php
+++ database/migrations/2024_10_23_162443_create_user_images_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('user_images', function (Blueprint $table) {
             $table->id();
             $table->uuid('uid')->unique();
-            $table->string('user_uid');
+            $table->uuid('user_uid');
             $table->string('name')->nullable();
             $table->string('type')->nullable();
             $table->integer('size')->nullable();
