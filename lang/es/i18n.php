b71ebbcc8aab86de0ce21e92c1a67cef1a1d74bd
frandg06 <fdiez86@gmail.com>
2025-04-15 00:56:58 +0200

    ✨ feat(i18n): añadir nuevos mensajes de error para eventos y tickets

--- lang/es/i18n.php
+++ lang/es/i18n.php
@@ -81,6 +81,10 @@ return [
     'base' => 'Has recibido un :interaction.',
     'hook' => 'Has hecho un Hook.',
     'message' => ':username te ha enviado un mensaje.',
-  ]
-
+  ],
+  'event_not_found' => 'El evento no existe o no está activo.',
+  'event_is_past_delete' => 'No se puede eliminar un evento que ya ha finalizado.',
+  'event_is_past' => 'El evento ya ha finalizado.',
+  'ticket_limit_reached' => 'Has alcanzado el límite de tickets para este evento.',
+  'ticket_not_found' => 'El ticket no existe o no está activo.',
 ];
