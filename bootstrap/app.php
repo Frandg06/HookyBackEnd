6d547232ca15d7acec825d5e2d5319dd92ae401c
frandg06 <fdiez86@gmail.com>
2025-04-14 20:18:19 +0200

    🔨 refactor(app): eliminar línea en blanco innecesaria y mejorar la estructura del código

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,7 +26,6 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-
         if (env('APP_ENV') !== 'local') {
             $exceptions->render(function (AuthenticationException $e, Request $request) {
                 return response()->json(['custom_message' => 'No existe una sesion activa', 'type' => 'AuthException'], 401);
