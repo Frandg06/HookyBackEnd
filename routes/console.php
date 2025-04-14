f215fbc61d6244823b6b61f8c9e0f9ca0a9b3071
frandg06 <fdiez86@gmail.com>
2025-04-15 01:16:21 +0200

    🔨 refactor(console): renombrar comando 'prod' a 'check:env' y actualizar su propósito

--- routes/console.php
+++ routes/console.php
@@ -7,8 +7,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-
-
-Artisan::command('prod', function () {
+Artisan::command('check:env', function () {
     $this->comment(app()->isProduction() ? 'Production' : 'Development');
-})->purpose('Display an inspiring quote')->hourly();
+})->purpose('Check if the app is in production or development environment');
