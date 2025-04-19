d108f1c4ff6ba4a5eef6ef7a1a1cebac49c55f0f
frandg06 <fdiez86@gmail.com>
2025-04-19 04:23:44 +0200

    ✨ feat(command): añadir comando para crear una empresa de demostración

---
+++ app/Console/Commands/CreateDemoCompany.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Company;
+use App\Models\TimeZone;
+use Illuminate\Console\Command;
+
+class CreateDemoCompany extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:create-company';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Company::create([
+            'uid' => '54ce8856-fb28-4ff9-bae5-6ed039829959',
+            'name' => 'Empresa de Prueba',
+            'email' => 'demo@hookyapp.es',
+            'password' => 'Demo2025',
+            'timezone_uid' => TimeZone::find(2)->uid,
+            'pricing_plan_uid' => \App\Models\PricingPlan::find(4)->uid
+        ]);
+    }
+}
