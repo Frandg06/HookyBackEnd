49855ce625da00cfa373e1b7f7d452eaeeaef5a7
frandg06 <fdiez86@gmail.com>
2025-12-17 13:19:53 +0100

    fix(cors): eliminar el uso de '*' en 'allowed_origins' para mayor seguridad

--- config/reverb.php
+++ config/reverb.php
@@ -87,7 +87,6 @@ return [
                 'allowed_origins' => [
                     'https://admin.hookyapp.es',
                     'https://app.hookyapp.es',
-                    '*',
                 ],
                 'ping_interval' => env('REVERB_APP_PING_INTERVAL', 60),
                 'activity_timeout' => env('REVERB_APP_ACTIVITY_TIMEOUT', 30),
