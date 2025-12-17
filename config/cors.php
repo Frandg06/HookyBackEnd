2c6419c7d89f2d262b245df76ada6e19864f5df5
frandg06 <fdiez86@gmail.com>
2025-12-17 14:07:30 +0100

    fix(cors): actualizar 'allowed_origins' para permitir configuraciones personalizadas

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => array_map('trim', explode(',', env('CORS', ''))),
 
     'allowed_origins_patterns' => [],
 
