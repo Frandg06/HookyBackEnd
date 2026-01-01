ab1493d9d1e7e1a9c75796f4e3fc4acec9aa0b5f
frandg06 <fdiez86@gmail.com>
2026-01-01 19:28:35 +0100

    feat(interactions): agregar soporte para paginación en el historial de interacciones

--- app/Http/Controllers/Customer/TargetUser/GetInteractionsGivenHistoryController.php
+++ app/Http/Controllers/Customer/TargetUser/GetInteractionsGivenHistoryController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Customer\TargetUser;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Container\Attributes\CurrentUser;
 use App\Actions\Customer\TargetUser\GetInteractionsGivenHistoryAction;
@@ -14,13 +15,13 @@ final class GetInteractionsGivenHistoryController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(#[CurrentUser] User $user, GetInteractionsGivenHistoryAction $action)
+    public function __invoke(#[CurrentUser] User $user, Request $request, GetInteractionsGivenHistoryAction $action)
     {
         if (! $user->is_premium) {
             return $this->errorResponse('This feature is only available for premium users', '/home', 403);
         }
 
-        $users = $action->execute($user);
+        $users = $action->execute($user, $request->integer('page', 1));
 
         return $this->successResponse('Interactions history retrieved successfully', $users);
     }
